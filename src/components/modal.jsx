3b51d2a20b1d0ac484f609c44c03705e7e31fb92
Abu sayed khan <sdbiplop@gmail.com>
2024-08-30 11:22:42 +0600

    ydjy

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,8 +1,25 @@
-
+import PropTypes from 'prop-types';
 import './member.css';
-const Modal = () => {
-    return 1;
+
+const Modal = ({ show, onClose, children }) => {
+    if (!show) {
+        return null;
+    }
+
+    return (
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <button className="modal-close" onClick={onClose}>X</button>
+                {children}
+            </div>
+        </div>
+    );
 };
 
+Modal.propTypes = {
+    show: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+};
 
 export default Modal;
